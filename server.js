65d6c3ff15f89651ac3c957f5c1ec78d3e725b28
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-01 10:58:54 -0400

    adds middleware

--- server.js
+++ server.js
@@ -3,20 +3,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const fruits = require("./models/fruits.js");
+//include the method-override package
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({extended: false}));
+
+//after app has been defined
+//use methodOverride.  We'll be adding a query parameter to our delete form named _method
+app.use(methodOverride("_method"))
+
 
 // DEFINE OUR ROUTES
 app.get("/fruits/", (req, res)=>{
     res.render("index.ejs", {
       allFruits: fruits
-    })
+    });
 });
 
 //put this above your show.ejs file
 app.get("/fruits/new", (req, res) => {
   res.render("new.ejs")
 })
+
+app.delete("/fruits/:indexOfFruitsArray", (req, res) => {
+  fruits.splice(req.params.indexOfFruitsArray, 1) //remove the item from the array
+  res.redirect("/fruits") //redirect back to index route
+});
 
 app.post("/fruits", (req, res) => {
   if(req.body.readyToEat === "on"){
@@ -32,7 +44,9 @@ app.get("/fruits/:indexOfFruitsArray", (req, res)=>{
     res.render("show.ejs", {
       fruit: fruits[req.params.indexOfFruitsArray]
     })
-})
+});
+
+
 
 // TELL OUR APP TO LISTEN ON PORT...
 app.listen(port, ()=>{
